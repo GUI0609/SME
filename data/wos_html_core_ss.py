f58a14bb87ad49c05769e0d21bfa00ed6f4b35a9
Guo <374058832@qq.co>
2022-10-31 23:05:04 +0800

    修改process_html_excel，删除重复的doi，对于冲突的doi用crossref修正

--- data/wos_html_core_ss.py
+++ data/wos_html_core_ss.py
@@ -3,23 +3,33 @@ from vespid.data.crossref import *
 from vespid.data.make_dataset import *
 from semanticscholar import SemanticScholar
 import crossref_commons
+from collections import Counter
+import numpy as np
 
 api_key = 'H6LFRtg5Ar55rlCiBkr1k7YH2nr6DCvpa538wF5C'
 
 def process_html_excel(email = None,htmldata_path=None):
+    #对于没有doi的文献，通过crossref检索标题添加doi号
     df = pd.read_excel(htmldata_path)
     df = df.reset_index().drop(columns = ['index'])
-    df.columns = ['title','source','author_name','abstract','Citation_Index','date','tt_ab','exp_theory_label','doi','pdf']
-    #对于没有doi的文献，通过crossref检索标题添加doi号
+    error_doi=[]
+    for (i,j) in Counter(df['Doi'].tolist()).items():
+        if j>1:
+            error_doi.append(i)
+    for index in range(len(df)):
+        if str(df.loc[index,'Doi']) in error_doi:
+          df.loc[index,'Doi'] = np.nan
+    
+    df.columns = ['title','source','author_names','abstract','Citation_Index','date','tt_ab','exp_theory_label','DOI','pdf']
     df = add_publication_dois(df,email,score_difference_threshold=0.20,n_jobs=None)
     df = df[pd.notnull(df['DOI'])].reset_index().drop(columns = ['index'])
     return df
 
 def concat_html_core(html_df,core_df):
-    html_df.columns = ['title','source','author_name','abstract','page_count','date','tt_ab','exp_theory_label','doi','pdf']
-    df = pd.merge(html_df,core_df,how = 'inner',on = 'doi',suffixes = ['_html','_core'])
+    # html_df.columns = ['title', 'date', 'DOI', 'abstract', 'source', 'author_names','Citation_Index', 'tt_ab', 'exp_theory_label', 'pdf']
+    df = pd.merge(html_df,core_df,how = 'inner',on = 'DOI',suffixes = ['_html','_core'])
     column_to_drop = ['title_html','source_html','abstract_html','date_html']
-    df = df.drop(columns = column_to_drop).rename(columns = {'title_core':'title','source_core':'source','abstract_core':'abstract','date_core':'date'})
+    df = df.drop(columns = column_to_drop).rename(columns = {'title_core':'title','source_core':'source','abstract_core':'abstract','date_core':'date','page_count_html':'page_count','author_names_html':'author_names'})
     return df
 
 def add_ss_to_wos(df,api_key):
@@ -30,10 +40,10 @@ def add_ss_to_wos(df,api_key):
         max_concurrent_requests=10,
         n_jobs=-1
     ).sort_index()
-    output['authors'] = output['authors_ss']
+    output = output.rename(columns = {'authors':'authors_ss'})
     return output
 
-def add_crossref_ref_to_ss_wos(df,api_key,doicolumns = 'doi'):
+def add_crossref_ref_to_ss_wos(df,api_key,doicolumns = 'DOI'):
     def get_ss_ref(doi):
         try:
             sch = SemanticScholar(api_key=api_key)
@@ -61,11 +71,3 @@ def correct_without_year(df):
         incor_date['date'].loc[index] = result['created']['date-parts'][0][0]
     df2 = cor_date.append(incor_date).reset_index().drop(columns = ['index'])
     return df2
-
-
-
-
-    
-
-
-
